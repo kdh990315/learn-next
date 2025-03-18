72ac7e1d6aabc8a355ce27ec0cd148d5f5047b19
kim dong hyeon <kdh990315@naver.com>
2025-03-18 11:26:10 +0900

    3/18

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <>
+  <header>
+    <Link href={"/"}>index</Link>
+    &nbsp;
+    <Link href={"/search"}>search</Link>
+    &nbsp;
+    <Link href={"/book/1"}>book/1</Link>
+  </header>
+  <Component {...pageProps} />
+  </>;
+}
